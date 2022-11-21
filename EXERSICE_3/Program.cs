6446995525b2362e7bc5027c4e7b5c82c42ce209
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-11-21 16:02:31 +0700

    add menu

--- EXERSICE_3/Program.cs
+++ EXERSICE_3/Program.cs
@@ -76,12 +76,17 @@ namespace EXERSICE_3
         {
             try
             {
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.Write("");
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. View all the records in the list");
+                Console.WriteLine("2. Search for a record in the list");
+                Console.WriteLine("3. Display the first record in the list");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice (1-4):");
+                char ch = Convert.ToChar(Console.ReadLine());
+                switch (ch)
+                {
+
+                }
             }
         }
     }
